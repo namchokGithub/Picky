3a430e097bd6f87c6e1f9a8ed99cbfab23e4d772
Namchok <pech4751@gmail.com>
2020-02-25 10:03:54 +0700

    Test git command | Namchok-Teamleader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,7 +43,7 @@ export class AppComponent implements OnInit {
       icon: 'warning'
     }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = ['Namchok', 'Namchok 2', 'Namchok 3', 'Namchok 4', 'Namchok 6', 'Reminders'];
 
   constructor(
     private platform: Platform,
