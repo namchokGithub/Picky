a4a13d63117d63a26aa79d13c66d88b70c80f0e9
Namchok <pech4751@gmail.com>
2020-02-29 16:47:48 +0700

    Add page taps | Namchok-TL-DevMaster

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },  {
+    path: 'taps',
+    loadChildren: () => import('./taps/taps.module').then( m => m.TapsPageModule)
   }
+
 ];
 
 @NgModule({
