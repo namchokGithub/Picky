cf7bb7ffbf000e406cd9626220e396ebc90f49f3
Komsan Tesana <benaza009@hotmail.co.th>
2020-03-02 18:46:51 +0700

    Create imprement account page | Dev - Komsan

--- src/app/account/addaccount/addaccount.page.ts
+++ src/app/account/addaccount/addaccount.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router'
+import { NavController } from "@ionic/angular";
 
 @Component({
   selector: 'app-addaccount',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddaccountPage implements OnInit {
 
-  constructor() { }
+  private  nameaccount: string = "";
+ 
+  constructor(public navCtrl: NavController,private router: Router) { }
 
   ngOnInit() {
   }
 
+  back(){
+
+    this.router.navigate(['showaccount'])
+  }
+  
 }
--- src/app/account/showaccount/showaccount.page.ts
+++ src/app/account/showaccount/showaccount.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NavController } from "@ionic/angular";
+import { Router } from '@angular/router'
 @Component({
   selector: 'app-showaccount',
   templateUrl: './showaccount.page.html',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowaccountPage implements OnInit {
 
-  constructor() { }
+  constructor(public navCtrl: NavController,private router: Router) { }
 
   ngOnInit() {
   }
+  openAddAccount(){
 
+    console.log('Clcik')
+    this.router.navigate(['addaccount'])
+    
+  }
 }
