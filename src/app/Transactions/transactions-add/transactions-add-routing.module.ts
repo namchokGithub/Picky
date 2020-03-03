c0ac89794d3b58f8d1065ca5b77ab6a963656432
Namchok <pech4751@gmail.com>
2020-03-03 11:23:31 +0700

    Update List | NamchokTL-Devmaster

--- src/app/Transactions/transactions-add/transactions-add-routing.module.ts
+++ src/app/Transactions/transactions-add/transactions-add-routing.module.ts
@@ -1,20 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-<<<<<<< HEAD:src/app/Transactions/transactions-add/transactions-add-routing.module.ts
 import { TransactionsAddPage } from './transactions-add.page';
-=======
-import { ListTransactionsPage } from './list-transactions.page';
->>>>>>> origin/Jutamas:src/app/Transactions/transactions-add/list-transactions/list-transactions-routing.module.ts
 
 const routes: Routes = [
   {
     path: '',
-<<<<<<< HEAD:src/app/Transactions/transactions-add/transactions-add-routing.module.ts
     component: TransactionsAddPage
-=======
-    component: ListTransactionsPage
->>>>>>> origin/Jutamas:src/app/Transactions/transactions-add/list-transactions/list-transactions-routing.module.ts
   }
 ];
 
@@ -22,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class ListTransactionsPageRoutingModule {}
+export class TransactionsPageRoutingModule {}
