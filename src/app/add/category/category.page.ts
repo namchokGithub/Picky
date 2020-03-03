e5832d60890ec98146026faacc686b1ecbdaf264
Namchok <pech4751@gmail.com>
2020-03-04 06:46:47 +0700

    Update all | Namchok

--- src/app/add/category/category.page.ts
+++ src/app/add/category/category.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 
 @Component({
@@ -8,15 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryPage implements OnInit {
 
-  constructor() { }
+  constructor(private nav: NavController, private router: Router) { }
     public category = [
-      {title:'Bonus'},
-      {title:'Lotter'},
-      {title:'Salary'},
-      {title:'Tips'},
-      {title:'Others'}
-    ]
+      {title: 'Bonus'},
+      {title: 'Lotter'},
+      {title: 'Salary'},
+      {title: 'Tips'},
+      {title: 'Others'}
+    ];
+
   ngOnInit() {
   }
-  
+
+  back() {
+    this.router.navigate(['add'], { replaceUrl: true });
+  }
 }
