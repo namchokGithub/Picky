1cac02d72d5bb8d236f024b17e685dbdf7c09a9e
Varthinee <varthinee427@gmail.com>
2020-03-04 14:43:25 +0700

    Create Enterprisemanagement | Varthinee PM

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -50,7 +50,8 @@ const routes: Routes = [
   {
     path: 'category',
     loadChildren: () => import('./add/category/category.module').then( m => m.CategoryPageModule)
-  },  {
+  },
+  {
     path: 'enterprisemanagement',
     loadChildren: () => import('./account/enterprisemanagement/enterprisemanagement.module').then( m => m.EnterprisemanagementPageModule)
   }
