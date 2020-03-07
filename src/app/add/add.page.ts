f6bce6d98651c576122e9cfd596203df811ee0cb
Phannita Khanthason <60160345@go.buu.ac.th>
2020-03-07 19:14:19 +0700

    Add async function addtransaction | Phannita-QM

--- src/app/add/add.page.ts
+++ src/app/add/add.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController, NavController } from '@ionic/angular';
+import { ModalController, NavController, AlertController } from '@ionic/angular';
 import { ActivatedRoute, Router } from '@angular/router';
 
 
@@ -12,10 +12,15 @@ export class AddPage implements OnInit {
   
   income :string = ""
   expense :string = ""
+  cash :string = ""
+  category :string = ""
+  date :string = ""
+  note :string = ""
+
   constructor(private nav: NavController,
               private modalController: ModalController, 
               public activatedRoute: ActivatedRoute,
-              private router: Router) { }
+              private router: Router,public alertController: AlertController) { }
 
   ngOnInit() {
     this.activatedRoute.queryParamMap.subscribe(params => {
@@ -39,5 +44,22 @@ export class AddPage implements OnInit {
   confirm() {
     this.router.navigate(['home'], { replaceUrl: true });
   }
- 
+
+  async addtransaction() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async add(){
+    this.cash;
+    this.category;
+    this.date;
+    this.note;
+  }
 }
