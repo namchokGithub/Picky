d191831fcc9f4be7f4b4ff15a4f7a2595ce965e4
Jutamas <juice.jutamas@gmail.com>
2020-03-07 15:44:03 +0700

    Create function editname | Jutamas-QA

--- src/app/setting/setting.page.ts
+++ src/app/setting/setting.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-setting',
@@ -7,25 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingPage implements OnInit {
   status=true
-  name = 'Mhee'
-  constructor() { }
+  name:string
+  alertCtrl: AlertController
+  constructor() { 
+      this.name = 'Mhee'
+  }
 
   ngOnInit(){
-
+    
   }
 
   editname(){
     console.log(123)
+    const confirm = this.alertCtrl.create({
+      title:'การแก้ไข',
+      message: 'ต้องการแก้ไขหรือไม่',
+      buttons:[
+        {
+          text: 'ใช่',
+          handler:() =>{
+            console.log('กดปุ่มแก้ไข');
+          }
+        },
+        {
+          text: 'ไม่',
+          handler:() =>{
+            console.log('ยกเลิกการแก้ไขแก้ไข');
+          }
+        }
+      ]
+    })
+
+
     if(this.status == true){
         this.status = false
     }else{
         this.status = true
     }
   }
-  successtoedit(){
-    if(this.status == false){
 
-    }
+  successtoEdit(){
+
   }
 
 }
