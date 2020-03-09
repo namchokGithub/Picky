29d3c0d0f82daedce0fd8b1f46d6c076ca468eb1
Komsan Tesana <benaza009@hotmail.co.th>
2020-03-10 04:29:28 +0700

    สร้างการทำงานของการ add ค่า ส่งข้อมูลลงฐานข้อมูล | Dev - komsan

--- src/app/add/add.page.ts
+++ src/app/add/add.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController, NavController, AlertController } from '@ionic/angular';
+import { ModalController, NavController, AlertController ,ToastController,LoadingController} from '@ionic/angular';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgForOf } from '@angular/common';
 import { NgForm } from '@angular/forms';
+import { RecordService } from './../services/record.service'
 
 
 @Component({
@@ -12,30 +13,44 @@ import { NgForm } from '@angular/forms';
 })
 export class AddPage implements OnInit {
   
-  income :string = ""
-  expense :string = ""
-  cash :string = ""
-  category :string = ""
-  date :string = ""
-  note :string = ""
+  private type_category :string = " "
+  private cash :string = " "
+  private name_category :string = " "
+  private date :string = " "
+  private note :string = " "
 
   constructor(private nav: NavController,
               private modalController: ModalController, 
               public activatedRoute: ActivatedRoute,
-              private router: Router,public alertController: AlertController) { }
+              private router: Router,
+              public alertController: AlertController,
+              private loadingController: LoadingController,
+              private toastController: ToastController,
+              private RecordService: RecordService) { }
+
+// * @Function   : ngOnInit => รับค่าจากหน้า CategoryPage ที่ส่งมายังหน้า add
+// * @Author     : Komsan Tesana
+// * @Create Date: 10/3/2563
 
   ngOnInit() {
-    // this.activatedRoute.queryParamMap.subscribe(params => {
-    //    this.income  = params.get('Income')
-    //    this.expense  = params.get('Expense')
-    // });
+     this.activatedRoute.queryParamMap.subscribe(params => {
+        this.type_category  = params.get('Type_category')
+        this.name_category = params.get('record_name')
+     });
   }
 
-
+// * @Function   : goCategoryPage => ไปยังหน้า CategoryPage
+// * @Author     : Komsan Tesana
+// * @Create Date: 10/3/2563
   goCategoryPage() {
     this.router.navigate(['category']);
   }
 
+  
+// * @Function   : back => ย้อนไปยังหน้า home
+// * @Author     : Komsan Tesana
+// * @Create Date: 10/3/2563
+  
   back() {
     this.router.navigate(['home'], { replaceUrl: true });
   }
@@ -44,6 +59,9 @@ export class AddPage implements OnInit {
     this.router.navigate(['home'], { replaceUrl: true });
   }
 
+// * @Function   : addtransaction => แสดงหน้าต่างข้อความ
+// * @Author     : Komsan Tesana
+// * @Create Date: 10/3/2563
   async addtransaction() {
     const alert = await this.alertController.create({
       header: 'Alert',
@@ -55,11 +73,51 @@ export class AddPage implements OnInit {
     await alert.present();
   }
 
+// * @Function   : onSubmit => ส่งค่าจากที่ผู้ใช้กรอก บันทึกลงฐานข้อมูล
+// * @Author     : Komsan Tesana
+// * @Create Date: 10/3/2563
+
   onSubmit(){
-    
-      console.log(this.category);
-      console.log(this.date);
-      console.log(this.note);
-      console.log(this.cash);
+
+  
+      console.log("ประเภท :"+this.type_category);
+      console.log("ชื่อ :"+this.name_category);
+      console.log("วันที่ :"+this.date);
+      console.log("เพิ่มเติม :"+this.note);
+      console.log("จำนวนเงิน :"+this.cash);
+
+
+  }
+
+// * @Function   : validate => เช็คค่าหากไม่มีการกรอกข้อมูล จะทำการแสดงข้อความแจ้งเตือน
+// * @Author     : Komsan Tesana
+// * @Create Date: 10/3/2563
+
+  validate(){
+
+      if(this.type_category == " " ){
+
+        console.log("กรุณาระบุประเภทรายรับ-รายจ่าย")
+      }
+      else if(this.cash == " "){
+       
+        this.showToast('กรุณาระบุจำนวนเงิน')
+
+      }else if(this.date == " "){
+
+        this.showToast('กรุณาระบุวันที่')
+      }else{
+      this.onSubmit();
+      }
+  }
+
+// * @Function   : showToast => แสดงข้อความแจ้งเตือน
+// * @Author     : Komsan Tesana
+// * @Create Date: 10/3/2563
+  showToast(msg) {
+    this.toastController.create({
+      message: msg,
+      duration: 3000
+    }).then(toast => toast.present());
   }
 }
