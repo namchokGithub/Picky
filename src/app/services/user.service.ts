96fdd5c98e7e678fb35c6d0dc4c37e2974ac0411
kittisakdotcom <kittisak.ndp@gmail.com>
2020-03-09 20:53:10 +0700

    update ionic | Kittisak-DM

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -7,10 +7,11 @@ import {
 } from "@angular/fire/firestore";
 import { map, take } from "rxjs/operators";
 import { Observable } from "rxjs";
+import { async } from '@angular/core/testing';
 
 export interface User {
   id?: string;
-  user_email: string;
+  user_id: string;
   user_name: string;
   user_password: string;
   user_tel: string;
@@ -18,11 +19,11 @@ export interface User {
 
 @Injectable({
   providedIn: "root"
-})  
+})
 export class UserService {
   private user: Observable<User[]>;
   private user_Collection: AngularFirestoreCollection<User>;
-
+  private check:any[];
   constructor(private afs: AngularFirestore) {
     this.user_Collection = this.afs.collection<User>("user");
     this.user = this.user_Collection.snapshotChanges().pipe(
@@ -54,19 +55,30 @@ export class UserService {
   }
 
   add_user(User: User): Promise<DocumentReference> {
+    
     return this.user_Collection.add(User);
   }
 
   update_user(User: User): Promise<void> {
     return this.user_Collection.doc(User.id).update({
-      user_email: User.user_email,
+      user_email: User.user_id,
       user_name: User.user_name,
       user_password: User.user_password,
       user_tel: User.user_tel
-    }); 
+    });
   }
 
   delete_user(id: string): Promise<void> {
     return this.user_Collection.doc(id).delete();
+  }
+
+  check_user(user_id:string){
+    return this.user_Collection.doc<User>(user_id).valueChanges().pipe(
+        take(1),
+        map(user => {
+          user.user_id = user_id;
+          return user;
+        })
+      );
   }
 }
