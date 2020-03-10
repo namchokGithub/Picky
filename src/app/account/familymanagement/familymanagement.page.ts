dcb05feda46ef30f130539f07eac6149a3c894a8
OrmChomphunut <ormchomphunutorm@hotmail.com>
2020-03-10 14:28:35 +0700

    Create Function confirm | Chomphunut-SP

--- src/app/account/familymanagement/familymanagement.page.ts
+++ src/app/account/familymanagement/familymanagement.page.ts
@@ -39,4 +39,11 @@ export class FamilymanagementPage implements OnInit {
     //console.log(index)
     this.Name.splice(index, 1);
    }
+
+   //  Function: logout กด icon ออกจากระบบ เพื่อไปยังหน้า login
+  //  Name: Chomphunut 
+  //  Date: 6/3/20
+  confirm() {
+    this.router.navigate(['showaccount'], { replaceUrl: true });
+  }
 }
