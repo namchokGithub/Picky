d37024ef421901386dbadbea28cdd3e5a225aaf6
Komsan Tesana <benaza009@hotmail.co.th>
2020-03-10 20:24:45 +0700

    Update login | NamchokTL-in-Komsan

--- src/app/login/login/login.page.ts
+++ src/app/login/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, MenuController, AlertController ,ToastController } from "@ionic/angular";
-import { Router } from '@angular/router'
+import { NavController, MenuController, AlertController , ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
 
 @Component({
@@ -10,43 +10,44 @@ import { UserService } from '../../services/user.service';
 })
 export class LoginPage implements OnInit {
 
-  private username = null 
-  private password = null
-  private db_user : any = [];
+  private username = null;
+  private password = null;
+  private db_user: any = [];
+  private userlogin: any = [];
 
   constructor(
     public navCtrl: NavController
-    ,private router: Router
-    ,private menu: MenuController
-    ,public alertController: AlertController
-    ,private UserService: UserService
-    ,private toastController: ToastController
+    , private router: Router
+    , private menu: MenuController
+    , public alertController: AlertController
+    , private UserService: UserService
+    , private toastController: ToastController
   ) {
 
   }
 
   ngOnInit() {
     this.UserService.get_user().subscribe(async res => {
-      console.log(res);
+      // console.log(res);
 
       this.db_user = res;
-      
-     
+
+
     });
 
-    this.loginMenu()
+    this.loginMenu();
 
 
   }
 
-  goHomePage(user_name:string,user_password:string,user_id:string){
-   
-    this.router.navigate(['app'], {queryParams: {user_name:user_name,user_password:user_password,user_id:user_id}});
-    this.router.navigate(['home'], {queryParams: {user_name:user_name,user_password:user_password,user_id:user_id}});
+  goHomePage(user_name: string, user_password: string, user_id: string) {
+
+    this.router.navigate(['app'], {queryParams: {user_name, user_password, user_id}});
+    this.router.navigate(['home'], {queryParams: {user_name, user_password, user_id}});
   }
 
-  register(){
-    this.router.navigate(['register'])
+  register() {
+    this.router.navigate(['register']);
   }
 
   /**
@@ -54,34 +55,48 @@ export class LoginPage implements OnInit {
    * Name: Komsan tesana
    * 2020-03-10
    */
-    validate() {
+    async validate() {
+      if (this.validate_login()) {
+        await setTimeout( () => {
 
-        for (let i = 0; i <  this.db_user.length ; i++) {
-
-            if(this.username === this.db_user[i].user_id) {
-
-                if(this.password === this.db_user[i].user_password){
-                    this.showToast('เข้าสู่ระบบสำเร็จ')
-                    this.goHomePage(this.db_user[i].user_name,this.db_user[i].user_password,this.db_user[i].user_id);
-                }else if(this.password === null || this.password != this.db_user[i].user_password){
-                    this.showToast('รหัสผู้ใช้งานไม่ถูกต้อง')
-                }
-                    
-            } else if(this.username === null){
-                this.showToast('ชื่อผู้ใช้งานไม่ถูกต้อง')
-            } else if(this.username != this.db_user[i].user_id){
-                if(this.password === null){
-                this.showToast('รหัสผู้ใช้งานไม่ถูกต้อง')
-                }
-                else if(this.password != this.db_user[i].user_password){
-                this.showToast('ไม่พบข้อมูลผู้ใช้งานอยู่ในระบบ')
-                }
-            }
+          this.userlogin = this.check_login();
             
-
-        }
+          if (this.userlogin) {
+            this.goHomePage(this.userlogin.user_name, this.userlogin.user_password, this.userlogin.user_id);
+          } else {
+            this.showToast('รหัสผู้ใช้งานไม่ถูกต้อง');
+          }
+  
+        }, 3000);
+      }
     }
 
+    // comment
+    validate_login() {
+      if ( this.username === '') {
+       this.showToast('กรุณาใส่ชื่อผู้ใช้')
+       return false;
+      }else if ( this.username === '') {
+        this.showToast('กรุณาใส่รหัสผ่าน')
+        return false;
+      }else {
+        return true;
+      }
+    }
+
+    // comment1
+    async check_login() {
+
+      this.userlogin = this.db_user.find(user => user.user_id === this.username);
+
+      if (this.userlogin.user_id === this.username) {
+        console.log('true');
+        return true;
+      } else {
+        console.log('false');
+        return false;
+      }
+    }
     /**
      * loginMenu
      * Name: Phannita
@@ -91,7 +106,7 @@ export class LoginPage implements OnInit {
         this.menu.enable(false, 'menuSilde');
     }
 
-  
+
     // * @Function   : showToast => แสดงข้อความแจ้งเตือน
     // * @Author     : Komsan Tesana
     // * @Create Date: 10/3/2563
@@ -100,7 +115,6 @@ export class LoginPage implements OnInit {
             message: msg,
             duration: 3000,
             color: 'dark'
-            
         }).then(toast => toast.present());
     }
 
