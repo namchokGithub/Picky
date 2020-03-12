24238316050bd2b8e233383ffe78747291c1f66f
Komsan Tesana <benaza009@hotmail.co.th>
2020-03-12 18:58:39 +0700

    check account | dev - komsan

--- src/app/account/showaccount/showaccount.page.ts
+++ src/app/account/showaccount/showaccount.page.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { VirtualTimeScheduler } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
-
+import { AccountPersonService } from './../../services/account-person.service';
+import { AccountFamilyService } from './../../services/account-family.service';
+import { AccountBusinessService } from './../../services/account-business.service';
+import { UserService } from './../../services/user.service';
 @Component({
   selector: 'app-showaccount',
   templateUrl: './showaccount.page.html',
@@ -11,18 +14,40 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 export class ShowaccountPage implements OnInit {
 
-
-  constructor(public navCtrl: NavController, private router: Router , private activatedRoute: ActivatedRoute ) { }
+  private userlogin: any = [];
+  
+  constructor(public navCtrl: NavController,
+     private router: Router, 
+     private activatedRoute: ActivatedRoute,
+     private accountPersonService: AccountPersonService,
+     private accountFamilyService: AccountFamilyService,
+     private accountBusinessService: AccountBusinessService,
+     private userService: UserService) { }
 
   /*balance = ['1000','2000','3000']
   name_account = ['บัญชีส่วนตัว', 'บัญชีเงินฝาก', 'บัญชีเงินเก็บ'];
   type_account = ['ครอบครัว','องค์กร','ผู้ใช้ทั่วไป']
   user_member = [0,1,2,3]*/
 
-  Data = [
-    'บัญชีส่วนตัว', 'บัญชีเงินฝาก', 'บัญชีเงินเก็บ'];
+  private Data: any [];
+  ngOnInit() {
+    this.userlogin = this.userService.get_session_user();
 
-  ngOnInit() {}
+      console.log(this.userlogin);
+    this.accountPersonService.get_account_person().subscribe(async res => {
+      this.check_account(res);
+    });
+
+    this.accountFamilyService.get_account_family().subscribe(async res => {
+      this.check_account(res);
+    });
+    this.accountBusinessService.get_account_business().subscribe(async res => {
+      this.check_account(res);
+    });
+
+    
+    
+  }
   /* ไปสู่หน้า Add Account */
   openAddAccount() {
     console.log('Click');
@@ -52,6 +77,10 @@ export class ShowaccountPage implements OnInit {
 
   gotomanagementFamily() {
     console.log('gotomanagementFamily');
+  }
+
+  check_account(data_account){ 
+
   }
 
 }
