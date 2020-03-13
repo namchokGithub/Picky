cf0c798b2e134cc3961175d124723e19d7332222
kittisakdotcom <kittisak.ndp@gmail.com>
2020-03-13 23:25:43 +0700

    Update homepage | kittisak-DM

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,6 +8,7 @@ import {
 } from "@ionic/angular";
 import { ActivatedRoute, Router } from "@angular/router";
 import { UserService, User } from "src/app/services/user.service";
+import { TransactionService,transaction } from 'src/app/services/transaction.service';
 import { from } from "rxjs";
 @Component({
   selector: "app-home",
@@ -17,20 +18,28 @@ import { from } from "rxjs";
 export class HomePage implements OnInit {
   private account_id:string;
   private account_name:string;
- 
+  private income:number;
+  private express:number;
+  private balance:number;
+  private account_type:string;
   private user_session: any = [];
-  private account_person: any = [];
-
+  private transaction:transaction[]
+  private value:boolean
   constructor(
     private menu: MenuController,
     private activatedRoute: ActivatedRoute,
     private router:Router,
-    private user: UserService
+    private user: UserService,
+    private transactionService :TransactionService
   ) {
     
   }
 
   ngOnInit() {
+    this.income = 0;
+    this.express = 0;
+    this.balance = 0;
+    this.value = true;
     this.activatedRoute.queryParamMap.subscribe(params => {
       this.account_id  = params.get('account_id')
       console.log(this.account_id)
@@ -44,15 +53,43 @@ export class HomePage implements OnInit {
 
   async load_session_user() {
     this.user_session = this.user.get_session_user();
-    
-    await this.load_account()
+    await this.get_transaction()
   }
 
-  load_account(){
-    //this.account_person = this.accountPersonService.get_account_person()
+  get_transaction(){
+    this.transactionService.get_transaction().subscribe( res=>{
+      if(res){
+        for(let i = 0;i< res.length; i++){
+          if(res[i].tran_account_id == this.account_id){
+            this.transaction[i] = res[i]
+          }
+        }
+        console.log(this.transaction);
+        this.setvalue()
+      }else{
+        this.value = false;
+      }
+     
+    })
   }
+
+  setvalue(){
+    for(let i = 0;i < this.transaction.length; i++){
+      if(this.transaction[i].tran_category_type == "income"){
+        this.income += parseInt(this.transaction[i].tran_amount)
+      }else{
+        this.express += parseInt(this.transaction[i].tran_amount)
+      }
+    }
+    this.balance = this.income - this.express;
+  } 
+  
+  
+
 
   add(){
     this.router.navigate(['add'], {queryParams: {account_id:this.account_id,account_name:this.account_name}});
   }
+
+ 
 }
