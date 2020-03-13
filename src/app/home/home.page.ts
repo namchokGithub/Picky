442723b121b75742837bec6190327fc8fe3a371d
kittisakdotcom <kittisak.ndp@gmail.com>
2020-03-14 00:47:19 +0700

    ๊Update home | kittisak-DM

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,6 +10,7 @@ import { ActivatedRoute, Router } from "@angular/router";
 import { UserService, User } from "src/app/services/user.service";
 import { TransactionService,transaction } from 'src/app/services/transaction.service';
 import { from } from "rxjs";
+import { async } from '@angular/core/testing';
 @Component({
   selector: "app-home",
   templateUrl: "./home.page.html",
@@ -19,11 +20,12 @@ export class HomePage implements OnInit {
   private account_id:string;
   private account_name:string;
   private income:number;
-  private express:number;
+  private Expense:number;
   private balance:number;
   private account_type:string;
   private user_session: any = [];
-  private transaction:transaction[]
+  private transaction = []
+  private tran = []
   private value:boolean
   constructor(
     private menu: MenuController,
@@ -37,40 +39,41 @@ export class HomePage implements OnInit {
 
   ngOnInit() {
     this.income = 0;
-    this.express = 0;
+    this.Expense = 0;
     this.balance = 0;
     this.value = true;
     this.activatedRoute.queryParamMap.subscribe(params => {
       this.account_id  = params.get('account_id')
-      console.log(this.account_id)
+      //console.log(this.account_id)
       this.account_name = params.get('account_name')
-      console.log(this.account_name)
+      //console.log(this.account_name)
    });
 
     this.menu.enable(true,"menuSilde");
     this.load_session_user();
+    this.get_transaction()
   }
 
-  async load_session_user() {
+  load_session_user() {
     this.user_session = this.user.get_session_user();
-    await this.get_transaction()
   }
 
   get_transaction(){
-    this.transactionService.get_transaction().subscribe( res=>{
-      if(res){
-        for(let i = 0;i< res.length; i++){
-          if(res[i].tran_account_id == this.account_id){
-            this.transaction[i] = res[i]
-          }
-        }
-        console.log(this.transaction);
-        this.setvalue()
-      }else{
-        this.value = false;
-      }
-     
+     this.transactionService.get_transaction().subscribe( res=>{
+      this.tran = res;
+      this.check_transaction()
     })
+  }
+
+  check_transaction(){
+   
+    for(let i = 0;i< this.tran.length; i++){
+      console.log(i+' '+this.tran[i].tran_account_id+' '+this.account_id)
+      if(this.tran[i].tran_account_id == this.account_id){
+        this.transaction[i] = this.tran[i]
+      }
+    }
+    this.setvalue()
   }
 
   setvalue(){
@@ -78,16 +81,19 @@ export class HomePage implements OnInit {
       if(this.transaction[i].tran_category_type == "income"){
         this.income += parseInt(this.transaction[i].tran_amount)
       }else{
-        this.express += parseInt(this.transaction[i].tran_amount)
+        this.Expense += parseInt(this.transaction[i].tran_amount)
       }
     }
-    this.balance = this.income - this.express;
+    this.balance = this.income - this.Expense;
   } 
   
   
 
 
   add(){
+    this.income = 0;
+    this.Expense = 0;
+    this.balance = 0;
     this.router.navigate(['add'], {queryParams: {account_id:this.account_id,account_name:this.account_name}});
   }
 
